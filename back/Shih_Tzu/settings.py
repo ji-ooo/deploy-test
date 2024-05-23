aeeffa5a84b26b45ddbdfee6bde7cf2d1d0865e5
최지우 <andyandy0409@naver.com>
2024-05-23 16:00:32 +0900

    cors

--- back/Shih_Tzu/settings.py
+++ back/Shih_Tzu/settings.py
@@ -39,6 +39,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://deploy-test-ha4z.onrender.com'
 ]
 
 # Application definition
